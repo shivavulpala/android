ab522257eec4e1ef818301526a9af612ba4c8062
vulpala <priya589@cs.utah.edu>
2017-10-24 16:54:29 -0600

    initial commit

---
+++ Myfirstapp/app/src/main/java/com/vulpala/myfirstapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.vulpala.myfirstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+        //setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_registration);
+        //setContentView(R.layout.activity_forgotpswd);
+        //setContentView(R.layout.activity_welcome);
+
+    }
+}
---
+++ Myfirstproject/app/src/main/java/com/example/harikannareddy/myfirstproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.vulpala.myfirstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_login);
+        //setContentView(R.layout.activity_registration);
+        //setContentView(R.layout.activity_forgotpswd);
+        //setContentView(R.layout.activity_welcome);
+
+    }
+}
